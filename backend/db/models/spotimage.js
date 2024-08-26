3ddc10e407c4dd87f25458291ed02bfc071ccafe
James-Jon27 <jamescjones818@gmail.com>
2024-08-26 11:58:17 -0600

    Seeder to TEXT

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       url: {
         allowNull: false,
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
       },
       preview: {
         allowNull: false,
